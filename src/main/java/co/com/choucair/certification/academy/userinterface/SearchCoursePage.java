eb2aedf7efcd98bb47908da6e85c38e76dae4a40
Jose Ballesteros <josebp0107@gmail.com>
2021-09-02 14:20:58 -0500

    First commit

---
+++ src/main/java/co/com/choucair/certification/academy/userinterface/SearchCoursePage.java
@@ -0,0 +1,18 @@
+package co.com.choucair.certification.academy.userinterface;
+
+import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.screenplay.targets.Target;
+import org.openqa.selenium.By;
+
+public class SearchCoursePage extends PageObject {
+    public static final Target BUTTON_UC = Target.the("Select Universidad Choucair")
+            .located(By.xpath("//div[@id='universidad']/a"));
+    public static final Target INPUT_COURSE = Target.the("Search the course")
+            .located(By.id("coursesearchbox"));
+    public static final Target BUTTON_GO = Target.the("Click on button Go")
+            .located(By.xpath("//button[@class='btn btn-secondary']"));
+    public static final Target SELECT_COURSE = Target.the("Metodologia Bancolombia")
+            .located(By.xpath("//section/div/div/div/h4/a"));
+    public static final Target NAME_COURSE = Target.the("Extract the course name")
+            .located(By.className("coursename"));
+}
---
+++ src/test/resources/features/academyChoucair.feature
@@ -0,0 +1,21 @@
+
+
+@stories
+Feature: Academy Choucair
+  As a user, I want to learn how to automate in screamplay at the Choucair Academy with the automation course
+
+  @scenario1
+  Scenario Outline: Search for a automation course
+    Given than brandon wants to learn automation at the academy Choucair
+      | strUser   | strPassword   |
+      | <strUser> | <strPassword> |
+    When he search for the course on the choucair academy platform
+      | strCourse   |
+      | <strCourse> |
+    Then he finds the course called
+      | strCourse   |
+      | <strCourse> |
+
+    Examples:
+      | strUser        | strPassword   | strCourse                |
+      | jballesterosp  | Choucair2021* | Metodología Bancolombia  |
